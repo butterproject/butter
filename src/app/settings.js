00e91f3ffc3a831b569a6acf9141bfc4f1737c5a
chtrinh <chris.chtrinh@gmail.com>
2017-02-10 00:36:57 -0800

    Auto seed torrents after player is closed by using cache torrent files
    'autoSeed' - starts up a forked process with a WebTorrent client which
    enables this feature. Torrent files are create and saved in tmp folder
    to be used later for seeding. Randomly selects files for join swarm.
    'seedLimit' - starting limit for number of torrents connected.

--- src/app/settings.js
+++ src/app/settings.js
@@ -169,6 +169,8 @@ Settings.automaticUpdating = true;
 Settings.events = true;
 Settings.minimizeToTray = false;
 Settings.bigPicture = false;
+Settings.autoSeed = false;
+Settings.seedLimit = 2;
 
 // Features
 Settings.activateTorrentCollection = false;
